87a198f1fd9aaabbfef2dd1b9e02d1a701cc6120
RakeshThakur26 <rakesh.thakur.mail99@gmail.com>
2021-09-21 17:32:16 +0530

    Optimized Logic

--- DictionaryDemo/DictionaryDemo/Program.cs
+++ DictionaryDemo/DictionaryDemo/Program.cs
@@ -18,31 +18,20 @@ namespace DictionaryDemo
             d.Add(5, "Payer 4");
 
             Console.WriteLine("Number of players : " + d.Count);
-
-            string temp = d[1];
-            string temp2 = d[5];
-
-            d.Remove(1);
-            d.Add(1, temp2);
-            d.Remove(5);
-            d.Add(5, temp);
-
-            //d.Add(1,temp2);
-            //d.Add(5, temp);
+            string temp = d[1];          
+            d[1] = d[5];
+            d[5] = temp;
 
             foreach (var item in d)
             {
                 Console.WriteLine("Key : " + item.Key + ", Value : " + item.Value);
             }
-
+            Console.WriteLine();
             Console.WriteLine("Winner is : " + d[1]);
-
             Console.WriteLine("Clearing all the data of Dictionary..!");
             d.Clear();
-                    
-
+    
             Console.ReadLine();
-
         }
     }
 }
