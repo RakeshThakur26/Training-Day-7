537559ecac2fffdde12eea319318d66a87f4ccb0
RakeshThakur26 <rakesh.thakur.mail99@gmail.com>
2021-09-22 14:03:09 +0530

    Task

---
+++ TasksAndParallelism/TasksAndParallelism/Example.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TasksAndParallelism
+{
+    class Example
+    {
+        public void Threading()
+        {
+            long totalSize = 0;
+            if (!Directory.Exists(@"C:\Training\Batch2Assignment\Batch2Assignment"))
+            {
+                Console.WriteLine("The directory does not exist.");
+                return; 
+            }
+            String[] files = Directory.GetFiles(@"C:\Training\Batch2Assignment\Batch2Assignment"); 
+            
+            Parallel.For(0, files.Length, index => 
+            { 
+                FileInfo fi = new FileInfo(files[index]); 
+                long size = fi.Length; 
+                Interlocked.Add(ref totalSize, size); 
+            }); 
+            Console.WriteLine("Directory '{0}':", @"C:\Training\Batch2Assignment\Batch2Assignment"); 
+            Console.WriteLine("{0:N0} files, {1:N0} bytes", files.Length, totalSize);
+        }
+    }
+}
--- TasksAndParallelism/TasksAndParallelism/Program.cs
+++ TasksAndParallelism/TasksAndParallelism/Program.cs
@@ -14,29 +14,41 @@ namespace TasksAndParallelism
         {
             Console.WriteLine("Enter a number : ");
             int n = Convert.ToInt32(Console.ReadLine());
+           
+            DateTime start = DateTime.Now;
+            Console.WriteLine("Current date and time : " + start.ToString());
 
             Console.WriteLine("Sequential Thread...");
-            for (int i = 1; i < n; i++)
+            for (int index = 1; index <= n; index++)
             {
-                if(i%2 == 0)
-                    Console.WriteLine("Even number : "+ i + ", Thread : " + Thread.CurrentThread.ManagedThreadId);
+                if (index % 2 == 0)
+                    Console.WriteLine("Even number : " + index + ", Thread : " + Thread.CurrentThread.ManagedThreadId);
                 else
-                    Console.WriteLine("Odd number  : " + i + ", Thread : " + Thread.CurrentThread.ManagedThreadId);
+                    Console.WriteLine("Odd number  : " + index + ", Thread : " + Thread.CurrentThread.ManagedThreadId);
 
                 Thread.Sleep(500);
             }
             Console.WriteLine();
             Console.WriteLine("Parallel Thread...");
 
-            Parallel.For(1, n,i =>
-            {
-                if (i % 2 == 0)
-                    Console.WriteLine("Even number : " + i + ", Thread : " + Thread.CurrentThread.ManagedThreadId);
-                else
-                    Console.WriteLine("Odd number  : " + i + ", Thread : " + Thread.CurrentThread.ManagedThreadId);
-                Thread.Sleep(500);
-            });
+            Parallel.For(1, n, index =>
+             {
+                 if (index % 2 == 0)
+                     Console.WriteLine("Even number : " + index + ", Thread : " + Thread.CurrentThread.ManagedThreadId);
+                 else
+                     Console.WriteLine("Odd number  : " + index + ", Thread : " + Thread.CurrentThread.ManagedThreadId);
+                 Thread.Sleep(500);
+             });
 
+            Console.WriteLine("\nCurrent Time : " + DateTime.Now.ToLongTimeString());
+            DateTime end = DateTime.Now;
+
+            TimeSpan timespan = end - start;
+            Console.WriteLine("Total time taken : " + timespan.TotalSeconds);
+
+
+            //Example example = new Example();
+            //example.Threading();
             Console.ReadLine();
           
         }
